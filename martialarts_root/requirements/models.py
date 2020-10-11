635054c07856415928299e84f66f3617b1728452
Juan R <62229144+juan-abr@users.noreply.github.com>
2020-10-11 16:01:23 -0400

    continued tinkering

--- martialarts_root/requirements/models.py
+++ martialarts_root/requirements/models.py
@@ -1,6 +1,5 @@
 from django.db import models
         
-        
 class Rank (models.Model):
     degree       = models.CharField(max_length = 20)
     color        = models.CharField(max_length = 20)
@@ -12,26 +11,9 @@ class Rank (models.Model):
     def __str__(self):
         return self.color
 
-    @property
-    def rank_categories(self):
-        return self.categories.all().order_by('position')
-
-
-# Individual requirements (i.e. Ki Cho 4, Middle Block, Horse Kick, etc.)
-class Requirement (models.Model):
-    title       = models.CharField(max_length=20)
-    position    = models.IntegerField()
-    video_url   = models.CharField(max_length=20, blank = True)
-    desc        = models.TextField(blank = True)
-    # category    = models.ForeignKey(Category)
-    # ranks       = models.ManyToManyField(Rank)
-
-    def __str__(self):
-        return self.title
-
     # @property
-    # def requirement_categories(self):
-    #     return self.category.order_by('position')
+    # def rank_categories(self):
+    #     return self.categories.all().order_by('position')
 
 # One category may have the same name but with different ranks
 class Category (models.Model):
@@ -50,3 +32,20 @@ class Category (models.Model):
     @property
     def cat_requirements(self):
         return self.requirements.all().order_by('position')
+
+
+# Individual requirements (i.e. Ki Cho 4, Middle Block, Horse Kick, etc.)
+class Requirement (models.Model):
+    title       = models.CharField(max_length=20)
+    position    = models.IntegerField()
+    video_url   = models.CharField(max_length=20, blank = True)
+    desc        = models.TextField(blank = True)
+    # category    = models.ForeignKey(Category)
+    # ranks       = models.ManyToManyField(Rank)
+
+    def __str__(self):
+        return self.title
+
+    # @property
+    # def requirement_categories(self):
+    #     return self.category.order_by('position')
