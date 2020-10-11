dc31f37690472193d1e06916b558515218e4fe87
Juan R <62229144+juan-abr@users.noreply.github.com>
2020-10-10 21:35:42 -0400

    syllabi removed

--- martialarts_root/martialarts_site/settings.py
+++ martialarts_root/martialarts_site/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'requirements.apps.RequirementsConfig',
-    'syllabi.apps.SyllabiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- martialarts_root/martialarts_site/urls.py
+++ martialarts_root/martialarts_site/urls.py
@@ -21,6 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('syllabi/', include('syllabi.urls')),
     path('requirements/', include('requirements.urls')),
 ]
