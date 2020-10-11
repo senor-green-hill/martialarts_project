a22ec5070382becb28482add4238ed4dad90b632
Juan R <62229144+juan-abr@users.noreply.github.com>
2020-10-11 15:49:14 -0400

    tinkering with the models

--- martialarts_root/requirements/models.py
+++ martialarts_root/requirements/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+        
+        
+class Rank (models.Model):
+    degree       = models.CharField(max_length = 20)
+    color        = models.CharField(max_length = 20)
+    position     = models.IntegerField()
+    eligibility  = models.TextField(blank = True)
+    # requirements = models.ManyToManyField(Requirement)
+    #categories  = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.color
+
+    @property
+    def rank_categories(self):
+        return self.categories.all().order_by('position')
+
 
 # Individual requirements (i.e. Ki Cho 4, Middle Block, Horse Kick, etc.)
 class Requirement (models.Model):
@@ -19,30 +36,17 @@ class Requirement (models.Model):
 # One category may have the same name but with different ranks
 class Category (models.Model):
     name            = models.CharField(max_length=20)
-    assoc_rank      = models.CharField(max_length=20)
     position        = models.IntegerField()
-    requirements    = models.ManyToManyField(Requirement)
-    #rank = models.ManyToManyField(Rank)
+    assoc_rank      = models.CharField(max_length=20)
+    #requirements    = models.ManyToManyField(Requirement)
+    ranks = models.ManyToManyField(Rank)
 
     def __str__(self):
-        return self.assoc_rank + ' ' + self.name
+        return self.ra1 self.name
+
+    @property
+    def category_ranks
 
     @property
     def cat_requirements(self):
         return self.requirements.all().order_by('position')
-        
-        
-class Rank (models.Model):
-    degree       = models.CharField(max_length = 20)
-    color        = models.CharField(max_length = 20)
-    position     = models.IntegerField()
-    eligibility  = models.TextField(blank = True)
-    # requirements = models.ManyToManyField(Requirement)
-    categories  = models.ManyToManyField(Category)
-
-    def __str__(self):
-        return self.color
-
-    @property
-    def rank_categories(self):
-        return self.categories.all().order_by('position')
