df7493a75662ae1619eb3e0218fa14ee5d3cdf7c
Juan R <62229144+juan-abr@users.noreply.github.com>
2020-10-11 21:59:41 -0400

    Added Media model

--- martialarts_root/requirements/models.py
+++ martialarts_root/requirements/models.py
@@ -1,51 +1,51 @@
 from django.db import models
-        
+
 class Rank (models.Model):
     degree       = models.CharField(max_length = 20)
     color        = models.CharField(max_length = 20)
     position     = models.IntegerField()
     eligibility  = models.TextField(blank = True)
-    # requirements = models.ManyToManyField(Requirement)
-    #categories  = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.color
 
-    # @property
-    # def rank_categories(self):
-    #     return self.categories.all().order_by('position')
-
-# One category may have the same name but with different ranks
+    @property
+    def categories(self):
+        return self.category_set.all().order_by('position')
+        
+# Categories for a Rank. (i.e. Yellow Belt Hand Techniques, Black Belt Hand Techniques)
 class Category (models.Model):
     name            = models.CharField(max_length=20)
     position        = models.IntegerField()
-    assoc_rank      = models.CharField(max_length=20)
-    #requirements    = models.ManyToManyField(Requirement)
-    ranks = models.ManyToManyField(Rank)
+    rank = models.ForeignKey(Rank, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.ra1 self.name
+        return "%s %s" % (self.rank, self.name)
 
     @property
-    def category_ranks
-
-    @property
-    def cat_requirements(self):
-        return self.requirements.all().order_by('position')
-
+    def requirements(self):
+        return self.requirement_set.all().order_by('position')
 
 # Individual requirements (i.e. Ki Cho 4, Middle Block, Horse Kick, etc.)
 class Requirement (models.Model):
     title       = models.CharField(max_length=20)
     position    = models.IntegerField()
-    video_url   = models.CharField(max_length=20, blank = True)
     desc        = models.TextField(blank = True)
-    # category    = models.ForeignKey(Category)
-    # ranks       = models.ManyToManyField(Rank)
+    category    = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
 
-    # @property
-    # def requirement_categories(self):
-    #     return self.category.order_by('position')
+    @property
+    def media(self):
+        return self.media_set.all().order_by('position')
+
+# Images, videos, etc. associated with a particular requirement.
+class Media (models.Model):
+    file_name   = models.CharField(max_length=20)
+    requirement = models.ForeignKey(Requirement, on_delete=models.CASCADE)
+    desc        = models.TextField(blank=True)
+    position    = models.IntegerField()
+
+    def __str__(self):
+        return "%s %s" % (self.requirement, self.file_name)
