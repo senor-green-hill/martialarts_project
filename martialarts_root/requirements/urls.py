c66434813c74d61cbff6ce98dcf856b0ef900404
Juan R <62229144+juan-abr@users.noreply.github.com>
2020-10-12 22:17:03 -0400

    requirement detail working

--- martialarts_root/requirements/urls.py
+++ martialarts_root/requirements/urls.py
@@ -5,6 +5,6 @@ from .views import RankListView, RankDetailView, CategoryDetailView, Requirement
 urlpatterns = [
     path('', RankListView.as_view(), name = 'rank_list_view'),
     path('rank/<int:rank_id>/', RankDetailView.as_view(), name = 'rank_detail_view'),
-    path('rank/<int:rank_id>/category/<int:category_id>', CategoryDetailView.as_view(), name = 'category_detail_view'),
     path('rank/<int:rank_id>/category/<int:category_id>/requirement/<int:requirement_id>', RequirementDetailView.as_view(), name = 'requirement_detail_view'),
+    path('categories/', CategoryDetailView.as_view(), name = 'category_detail_view'),
 ]
