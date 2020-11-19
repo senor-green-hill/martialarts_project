07057f04700de13e2fcff6e59fd4b67aa5b5f3f8
senor-green-hill <juanabarro@protonmail.com>
2020-11-19 13:12:44 -0500

    Eligibility model registered

--- martialarts_root/requirements/admin.py
+++ martialarts_root/requirements/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Requirement, Category, Rank, Media
+from .models import Requirement, Category, Rank, Media, Eligibility
 
 # Register your models here.
 admin.site.register(Requirement)
