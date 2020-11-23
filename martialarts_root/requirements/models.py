a3673809c55e35662c0ea3c46ebba1d5c2c69f57
Ubuntu <ubuntu@ip-172-31-42-133.ec2.internal>
2020-11-23 20:08:46 +0000

    blank slug is True

--- martialarts_root/requirements/models.py
+++ martialarts_root/requirements/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Rank (models.Model):
-    slug        = models.SlugField()
+    slug        = models.SlugField(blank = True)
     degree       = models.CharField(max_length = 20)
     color        = models.CharField(max_length = 20)
     position     = models.IntegerField()
