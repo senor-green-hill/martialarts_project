95a88f6942c46207304d5360b6833d36646953ca
senor-green-hill <juanabarro@protonmail.com>
2020-12-17 17:08:35 -0500

    added RequirementAdmin

--- martialarts_root/requirements/admin.py
+++ martialarts_root/requirements/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Requirement, Category, Rank, Media, Eligibility
 
 # Register your models here.
-
+class RequirementAdmin(admin.ModelAdmin):
+    list_display = ('name')
 
 admin.site.register(Requirement)
 admin.site.register(Category)
