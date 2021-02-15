ab0d687c771eec9548f13a64c29bc04e208ba603
senor-green-hill <juanabarro@protonmail.com>
2021-02-15 21:10:55 +0000

    updated requirements admin

--- martialarts_root/requirements/admin.py
+++ martialarts_root/requirements/admin.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from .models import Requirement, Category, Rank, Media, Eligibility
 
 # Register your models here.
+class RankAdmin(admin.ModelAdmin):
+    ordering = ('position')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('rank', 'name')
+    ordering = ('position')
+    search_fields = ('rank', 'name')
+
 class RequirementAdmin(admin.ModelAdmin):
     list_display = ('category', 'title')
 
 admin.site.register(Requirement, RequirementAdmin)
-admin.site.register(Category)
-admin.site.register(Rank)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Rank, RankAdmin)
 admin.site.register(Media)
 admin.site.register(Eligibility)
